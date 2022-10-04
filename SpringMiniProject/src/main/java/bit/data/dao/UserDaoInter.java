862efe4068e27ecfc6d25478fd9e28a12e6ecaed
j-a-27 <jinana27@gmail.com>
2022-10-04 17:01:32 +0900

    진아 10/04 17시 00분 studentleclist(중복파일) 삭제 layoutMain의 menu에 로그인하면 마이페이지 뜨도록 설정 UserController 관련 Dao, Service 생성 boardForm 수정

--- SpringMiniProject/src/main/java/bit/data/dao/UserDaoInter.java
+++ SpringMiniProject/src/main/java/bit/data/dao/UserDaoInter.java
@@ -2,9 +2,21 @@ package bit.data.dao;
 
 import bit.data.dto.UserDto;
 
+import java.util.List;
 import java.util.Map;
 
 public interface UserDaoInter {
+
+    public int getUserTotalCount();
+    public List<UserDto> getAllUsers();
+    public UserDto getDataByNum(int usernum);
+    public UserDto getDataById(String userid);
+
+    public void updateUserPhoto(Map<String, Object> map);
+    public void updateUser(UserDto dto);
+    public void deleteUser(int usernum);
+
+
 
 
 }
